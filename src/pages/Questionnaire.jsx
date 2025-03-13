62274e37f29ef004ad667ec822aa3e105cfa8e1b
mombeh nadine <nadinemombeh@gmail.com>
2025-03-13 13:14:24 +0100

    commit pages

---
+++ src/pages/Questionnaire.jsx
@@ -0,0 +1,50 @@
+import QuestionCard from "../components/QuestionCard";
+import { useState, useContext,useEffect } from "react";
+import { DataContext } from "../context/context";
+import { useNavigate } from "react-router";
+import {useParams} from 'react-router'
+
+export default function Questionnaire() {
+  const {number: num} =useParams()
+  const number = parseInt(num, 10)
+  const [time,setTime]=useState(10)
+  const { tabQuestions, setTabReponse, tabReponse } =
+    useContext(DataContext);
+  const navigate = useNavigate();
+
+  const handleNext = (question, response) => {
+    setTime(10)
+    const newQuestion = { ...question, yours: response };
+    setTabReponse([...tabReponse, newQuestion]);
+    if (number === 10) {
+     return navigate("/ScorePage");
+    }
+      navigate(`/Questionnaire/${number + 1}`);
+
+    }
+  
+
+  useEffect(()=>{
+    let t
+    if(time > 0){
+      t= setTimeout(()=>{
+        setTime(time -1)
+      },1000);
+    }else if(time === 0){
+      handleNext(tabQuestions[number],"")
+    }
+    return ()=>{
+      clearTimeout(t)
+    }
+  },[time])
+
+  return (
+    <QuestionCard
+      next={handleNext}
+      question={tabQuestions[number-1]}
+      totalQuestion={tabQuestions.length}
+      number={number}
+      time = {time}
+    />
+  );
+}
